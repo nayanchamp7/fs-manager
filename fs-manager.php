8661d8cad4963bd2105c78db561b5df6c087a358
nayanchamp7 <nazrulislamnayan7@gmail.com>
2023-10-07 20:19:08 +0600

    added textbox and apply message

--- fs-manager.php
+++ fs-manager.php
@@ -86,14 +86,37 @@ function fs_add_custom_checkbox_for_bundle_product()
     </script>
     <div class="fs-gift-box-input-wrapper">
         <div class="fs-gift-box-checkbox">
-            <input type="checkbox" name="gift_box"> <label for="gift_box">GIFT BOX</label>
+            <input type="checkbox" name="gift_box"> <label for="gift_box">Send this bundle as a gift</label>
         </div>
 
-        <textarea name="gift_box_message" maxlength="50" style="display:none;" id="gift_box_message" cols="30" rows="5"></textarea>
+        <div id="gift_box_message" style="display:none;">
+            <textarea name="gift_box_message" maxlength="50" cols="30" rows="5"></textarea>
+            <p>To send the product as a gift, please write your message here.</p>
+        </div>
     </div>
     <?php
 }
 add_action('woocommerce_before_add_to_cart_quantity', 'fs_add_custom_checkbox_for_bundle_product');
+
+// Add data to cart item
+add_filter( 'woocommerce_add_cart_item_data', 'fs_add_cart_item_data', 25, 2 );
+function fs_add_cart_item_data( $cart_item_data, $product_id ) {
+
+    if( isset( $_POST['gift_box_message'] ) ) {
+        $cart_item_data['custom_data']['gift_box_message'] = $_POST['gift_box_message'];
+    }
+
+    return $cart_item_data;
+}
+
+// Add order item meta.
+add_action( 'woocommerce_add_order_item_meta', 'fs_add_order_item_meta' , 10, 3 );
+function fs_add_order_item_meta ( $item_id, $cart_item, $cart_item_key ) {
+    if ( isset( $cart_item[ 'custom_data' ]['gift_box_message'] ) && !empty($cart_item[ 'custom_data' ]['gift_box_message']) ) {
+        $message = $cart_item[ 'custom_data' ]['gift_box_message'];
+        wc_add_order_item_meta( $item_id, __( "Message"), $message );
+    }
+}
 
 function fs_filter_backtoblog_link($html_link) 
 {
@@ -115,7 +138,13 @@ add_filter('login_site_html_link', 'fs_filter_backtoblog_link');
  */
 add_filter( 'woocommerce_get_price_suffix', 'fs_add_price_suffix', 99, 4 );
 function fs_add_price_suffix( $html, $product, $price, $qty ){
+    if( !is_product() ) {
+        return;
+    } 
+
     $html .= ' <img src="'. FSM_PLUGIN_URL . "/assets/images/vip-user.png" .'" alt="VIP User" />';
     return $html;
 }
+
+
 
